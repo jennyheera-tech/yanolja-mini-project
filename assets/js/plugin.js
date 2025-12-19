8f2795e10cefdc6db6d12ef7adf466375b821bc8
jennyheera-tech <jennyheera@gmail.com>
2025-12-19 14:26:47 +0900

    스타트

---
+++ assets/js/plugin.js
@@ -0,0 +1,23 @@
+// 플러그인 스크립트
+
+//visual 스와이프
+let visualSwiper = null;
+
+var swiper = new Swiper('.mySwiper', {
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+    },
+});
+
+//section1 스와이프
+
+ var swiper = new Swiper(".bsn-swiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+// gsap 스크립트 등
